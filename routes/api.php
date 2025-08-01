91b7a0224e08424119d040468936450eb9ef9c41
estefany <estefany17viveros@gmail.com>
2025-08-01 10:37:33 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,42 @@
 <?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\AuthController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+Route::apiResource('answers', answerController::class);
+Route::apiResource('averages', averageController::class);
+Route::apiResource('trainers', trainerController::class);
+Route::apiResource('veterinaries', veterinaryController::class);
+Route::apiResource('shelters', shelterController::class);
+Route::apiResource('pets', petController::class);
+Route::apiResource('adoptions', adoptionController::class);
+Route::apiResource('appointments', appointmentController::class);
+Route::apiResource('notifications', notificationController::class);
+Route::apiResource('requestts', requesttController::class);
+Route::apiResource('services', serviceController::class);
+Route::apiResource('shoppingcars', shoppingcarController::class);
+Route::apiResource('orders', orderController::class);
+Route::apiResource('shipments', shipmentController::class);
+Route::apiResource('categories', categoryController::class);
+Route::apiResource('products', productController::class);
+Route::apiResource('inventories', inventoryController::class);
+Route::apiResource('orderitmes', orderitemController::class);
+Route::apiResource('payments', paymentController::class);
+Route::apiResource('paymentmethos', paymentmethoController::class);
+Route::apiResource('users', UserController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+
+
 });
